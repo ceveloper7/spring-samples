10bb0c451ba23fbf06716645f8e7d135ea72ce51
Carlos V. <cevabarcvilla@gmail.com>
2026-01-15 11:18:32 -0300

    Java Persistence with Spring Data and Hibernate - Chapter 4 - Defining query methods with Spring Data JPA

--- springdatajpa/src/main/java/com/ceva/javapersistence/springdatajpa/model/User.java
+++ springdatajpa/src/main/java/com/ceva/javapersistence/springdatajpa/model/User.java
@@ -6,6 +6,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 @Entity
 @Table(name="users")
@@ -15,14 +16,19 @@ public class User {
     private Long id;
     private String username;
     private LocalDate registrationDate;
+    private String email;
+    private int level;
+    private boolean active;
 
-    public User(){}
+    public User() {
 
-    public User(String username){
+    }
+
+    public User(String username) {
         this.username = username;
     }
 
-    public User(String username, LocalDate registrationDate){
+    public User(String username, LocalDate registrationDate) {
         this.username = username;
         this.registrationDate = registrationDate;
     }
@@ -35,16 +41,40 @@ public class User {
         return username;
     }
 
-    public LocalDate getRegistrationDate() {
-        return registrationDate;
-    }
-
     public void setUsername(String username) {
         this.username = username;
     }
 
+    public LocalDate getRegistrationDate() {
+        return registrationDate;
+    }
+
     public void setRegistrationDate(LocalDate registrationDate) {
         this.registrationDate = registrationDate;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
     }
 
     @Override
@@ -54,5 +84,18 @@ public class User {
                 ", username='" + username + '\'' +
                 ", registrationDate=" + registrationDate +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id.equals(user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
--- springdatajpa/src/main/java/com/ceva/javapersistence/springdatajpa/repositories/UserRepository.java
+++ springdatajpa/src/main/java/com/ceva/javapersistence/springdatajpa/repositories/UserRepository.java
@@ -1,10 +1,32 @@
 package com.ceva.javapersistence.springdatajpa.repositories;
 
 import com.ceva.javapersistence.springdatajpa.model.User;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.List;
+
 
 /**
  * Repositorio para el entidad User
  */
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+    List<User> findAllByOrderByUsernameAsc();
+    List<User> findByRegistrationDateBetween(LocalDate start, LocalDate end);
+    List<User> findByUsernameAndEmail(String username, String email);
+    List<User> findByUsernameOrEmail(String username, String email);
+    List<User> findByUsernameIgnoreCase(String username);
+    List<User> findByLevelOrderByUsernameDesc(int level);
+    List<User> findByLevelGreaterThanEqual(int level);
+    List<User> findByUsernameContaining(String text);
+    List<User> findByUsernameLike(String text);
+    List<User> findByUsernameStartingWith(String start);
+    List<User> findByUsernameEndingWith(String end);
+    List<User> findByActive(boolean active);
+    List<User> findByRegistrationDateIn(Collection<LocalDate> dates);
+    List<User> findByRegistrationDateNotIn(Collection<LocalDate> dates);
+    User findFirstByOrderByUsernameAsc();
+    User findTopByOrderByRegistrationDateDesc();
 }
